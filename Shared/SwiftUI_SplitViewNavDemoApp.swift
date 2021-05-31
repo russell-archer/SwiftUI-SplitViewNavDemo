872d5f18b7f92d0751d00f1d04e3de7d805c013d
Russell Archer <russell.archer@mac.com>
2021-06-01 00:15:34 +0100

    Initial Commit

---
+++ Shared/SwiftUI_SplitViewNavDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SplitViewNavDemoApp.swift
+//  Shared
+//
+//  Created by Russell Archer on 01/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SplitViewNavDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
